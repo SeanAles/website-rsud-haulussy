1f70c047034d3d488ecba64eefe9953f25ebc3f6
simrshaulussy2 <simrshaulussy@gmail.com>
2024-02-19 08:36:57 +0900

    Adding 2 leaflet in promkes page

--- resources/views/visitor/fasilitas/promosi-kesehatan.blade.php
+++ resources/views/visitor/fasilitas/promosi-kesehatan.blade.php
@@ -8,9 +8,19 @@
             <h1>Promosi Kesehatan</h1>
         </div>
 
-        <div class="text-center">
-            <img src="{{ asset("visitor/assets/img/gallery/qr_code.png") }}" alt=""><br>
-            <a href="https://tiny.one/Edukasi-Haulussy" target="_blank" class="btn btn-info">Tekan Saya</a>
+       <div class="container">
+        <div class="row justify-content-center">
+            <div class="text-center col-12 col-sm-12 col-md-8 col-lg-4 mt-3">
+                <img src="{{ asset("visitor/assets/img/promkes/Kanker Anak Sedunia 1.png") }}" width="100%"  alt="Promkes 1"><br>
+            </div>
+            <div class="text-center col-12 col-sm-12 col-md-8 col-lg-4 mt-3">
+                <img src="{{ asset("visitor/assets/img/gallery/qr_code.png") }}" width="80%" alt="Promkes QR"><br>
+                <a href="https://tiny.one/Edukasi-Haulussy" target="_blank" class="btn btn-info">Tekan Saya</a>
+            </div>
+            <div class="text-center col-12 col-sm-12 col-md-8 col-lg-4 mt-3">
+                <img src="{{ asset("visitor/assets/img/promkes/Kanker anak Sedunia 2.png") }}" width="100%"   alt="Promkes 2"><br>
+            </div>
         </div>
+       </div>
     </section>
 @endsection
