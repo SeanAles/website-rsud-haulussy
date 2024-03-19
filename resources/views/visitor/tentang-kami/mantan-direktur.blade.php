a3416e346899928f4d3c897789056180bba4d587
simrshaulussy2 <simrshaulussy@gmail.com>
2024-03-19 09:21:23 +0900

    change dr rodrigo image and name in mantan direktur page

--- resources/views/visitor/tentang-kami/mantan-direktur.blade.php
+++ resources/views/visitor/tentang-kami/mantan-direktur.blade.php
@@ -2,6 +2,18 @@
 
 @section('title', 'Mantan Direktur')
 
+@section('style')
+    <style>
+    a.custom-link {
+        cursor: pointer;
+        text-decoration: none;
+        
+    }
+
+       
+    </style> 
+@endsection
+
 @section('content')
     <div class="text-center mb-5">
         <h1>Mantan Direktur</h1>
@@ -26,8 +38,10 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">1954 - 1959</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. L. Huliselan, ARTS</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#exampleModal">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. L. Huliselan, ARTS</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#exampleModal">Lihat
+                            Lebih Lanjut
+                        </a>
                     </div>
                 </div>
             </div>
@@ -62,8 +76,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">1959 - 1963</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Nn. Tamaela</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#example">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Nn. Tamaela</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#example">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -85,10 +100,11 @@
                     </div>
                 </div>
             </div>
-            
+
             <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4">
-                <div class="card h-100 shadow card-span rounded-3"><img class="card-img-top rounded-top-3"
-                        src="{{ asset('visitor/assets/img/direk/Direktur.png') }}" alt="news" />
+                <div class="card h-100 shadow card-span rounded-3">
+                    <img class="card-img-top rounded-top-3" src="{{ asset('visitor/assets/img/direk/Direktur.png') }}"
+                        alt="news" />
                     <div class="card-body"><span class="fs--1 text-primary me-3">Direktur</span>
                         <svg class="bi bi-calendar2 me-2" xmlns="http://www.w3.org/2000/svg" width="12" height="12"
                             fill="currentColor" viewBox="0 0 16 16">
@@ -98,8 +114,8 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">Maret 11, 1954</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. H. Moehammad Moestafa</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#exa">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. H. Moehammad Moestafa</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#exa">Lihat Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -139,8 +155,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">1963 - 1968</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. R. Soebekti</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#ex">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. R. Soebekti</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#ex">Lihat Lebih
+                            Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -178,8 +195,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">1970 - 1972</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. M. Paliama</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#e">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. M. Paliama</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#e">Lihat Lebih
+                            Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -217,8 +235,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">1972 - 1975</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. M. D. Kharie</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. M. D. Kharie</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -256,8 +275,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">1975 - 1976</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. D. Palekahelu</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes1">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. D. Palekahelu</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes1">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -295,8 +315,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">1976 - 1983</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. J. Nahumury</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes2">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. J. Nahumury</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes2">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -334,8 +355,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">1983 - 1990</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. A. F. Lokollo, MPH</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes3">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. A. F. Lokollo, MPH</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes3">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -373,8 +395,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">1990 - 2001</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. A. Afifudin</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes4">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. A. Afifudin</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes4">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -412,8 +435,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">2001 - 2008</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. J. Manuputty, MPH</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes5">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. J. Manuputty, MPH</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes5">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -441,7 +465,8 @@
 
             <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4">
                 <div class="card h-100 shadow card-span rounded-3"><img class="card-img-top rounded-top-3"
-                        src="{{ asset('visitor/assets/img/direk/dr.  Henriette Tanamal, SpKK, MKes.png') }}" alt="news" />
+                        src="{{ asset('visitor/assets/img/direk/dr.  Henriette Tanamal, SpKK, MKes.png') }}"
+                        alt="news" />
                     <div class="card-body"><span class="fs--1 text-primary me-3">Direktur</span>
                         <svg class="bi bi-calendar2 me-2" xmlns="http://www.w3.org/2000/svg" width="12"
                             height="12" fill="currentColor" viewBox="0 0 16 16">
@@ -451,8 +476,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">2008</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Henriette Tanamal, Sp.KK, M.Kes.</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes6">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Henriette Tanamal, Sp.KK, M.Kes.</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes6">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -490,8 +516,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">2008 - 2012</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. F. Koedoeboen, M.Kes.</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes7">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. F. Koedoeboen, M.Kes.</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes7">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -529,8 +556,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">2012 - 2014</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Sri Ananta Widhya, M.Kes.</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes8">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Sri Ananta Widhya, M.Kes.</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes8">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -567,8 +595,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">2014 - 2022</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Justini Pawa, M.Kes.</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes9">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Justini Pawa, M.Kes.</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes9">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -605,8 +634,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">2019 - 2020</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Ritha Tahitu, M. Kes.</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes001">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Ritha Tahitu, M. Kes.</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes001">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -633,7 +663,8 @@
 
             <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4">
                 <div class="card h-100 shadow card-span rounded-3"><img class="card-img-top rounded-top-3"
-                        src="{{ asset('visitor/assets/img/direk/dr. Rodridgo Limmon, Sp.THT. MARS (Fix).png') }}" alt="news" />
+                        src="{{ asset('visitor/assets/img/direk/dr. Rodrigo Limmon, Sp.THT. MARS.png') }}"
+                        alt="news" />
                     <div class="card-body"><span class="fs--1 text-primary me-3">Direktur</span>
                         <svg class="bi bi-calendar2 me-2" xmlns="http://www.w3.org/2000/svg" width="12"
                             height="12" fill="currentColor" viewBox="0 0 16 16">
@@ -643,8 +674,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">2020 - 2021</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Rodridgo Limmon, Sp.THT, MARS</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes002">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Rodrigo Limmon, Sp.THT, MARS</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes002">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -661,7 +693,7 @@
                         </div>
                         <div class="text-center">
                             <div class="modal-body">
-                                <img src="{{ asset('visitor/assets/img/direk/dr. Rodridgo Limmon, Sp.THT. MARS (Fix).png') }}"
+                                <img src="{{ asset('visitor/assets/img/direk/dr. Rodrigo Limmon, Sp.THT. MARS.png') }}"
                                     style="max-height: 500px; max-width: 300px;">
                             </div>
                         </div>
@@ -671,7 +703,8 @@
 
             <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4">
                 <div class="card h-100 shadow card-span rounded-3"><img class="card-img-top rounded-top-3"
-                        src="{{ asset('visitor/assets/img/direk/dr. Zulkarnaini MS, Sp.JP, FIHA.png') }}" alt="news" />
+                        src="{{ asset('visitor/assets/img/direk/dr. Zulkarnaini MS, Sp.JP, FIHA.png') }}"
+                        alt="news" />
                     <div class="card-body"><span class="fs--1 text-primary me-3">Direktur</span>
                         <svg class="bi bi-calendar2 me-2" xmlns="http://www.w3.org/2000/svg" width="12"
                             height="12" fill="currentColor" viewBox="0 0 16 16">
@@ -681,8 +714,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">2021 - 2022</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Zulkarnaini MS, Sp.JP, FIHA</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes003">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Zulkarnaini MS, Sp.JP, FIHA</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes003">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
@@ -719,8 +753,9 @@
                             <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z">
                             </path>
                         </svg><span class="fs--1 text-900">2022 - 2023</span><span class="fs--1"></span>
-                        <h5 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Nazaruddin, M.Sc.</h5>
-                        <a class="stretched-link" role="button" data-bs-toggle="modal" data-bs-target="#tes004">Lihat Lebih Lanjut</a>
+                        <h6 class="font-base fs-lg-0 fs-xl-1 my-3">dr. Nazaruddin, M.Sc.</h6>
+                        <a class="custom-link" data-bs-toggle="modal" data-bs-target="#tes004">Lihat
+                            Lebih Lanjut</a>
                     </div>
                 </div>
             </div>
