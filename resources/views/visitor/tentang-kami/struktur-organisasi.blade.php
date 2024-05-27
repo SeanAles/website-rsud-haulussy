bb21130e39729b280ef2f58cb59bed9a62c28ec8
simrshaulussy2 <simrshaulussy@gmail.com>
2024-05-27 12:14:31 +0900

    Change struktur organisasi diagram (4)

--- resources/views/visitor/tentang-kami/struktur-organisasi.blade.php
+++ resources/views/visitor/tentang-kami/struktur-organisasi.blade.php
@@ -7,7 +7,7 @@
         <h1>Struktur Organisasi</h1>
     </div>
     <div class="container">
-        <img src="{{ asset('visitor/assets/img/diagram/Diagram Struktur Organisasi RS (27 Mei 2024) (3).png') }}" alt="" width="100%" height="100%">
+        <img src="{{ asset('visitor/assets/img/diagram/Diagram Struktur Organisasi RS (27 Mei 2024) (4).png') }}" alt="" width="100%" height="100%">
         <p class="text-danger">
             <b>*Diperbarui 27 Mei 2024</b>
         </p>
