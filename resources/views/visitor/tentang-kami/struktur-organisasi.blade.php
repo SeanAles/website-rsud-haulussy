86cffa05cbb387b0a97b178699acafc18d48acea
simrshaulussy2 <simrshaulussy@gmail.com>
2024-05-28 12:13:57 +0900

    change date updated of struktur organisasi diagram

--- resources/views/visitor/tentang-kami/struktur-organisasi.blade.php
+++ resources/views/visitor/tentang-kami/struktur-organisasi.blade.php
@@ -9,7 +9,7 @@
     <div class="container">
         <img src="{{ asset('visitor/assets/img/diagram/Diagram Struktur Organisasi RS (27 Mei 2024) (4).png') }}" alt="" width="100%" height="100%">
         <p class="text-danger">
-            <b>*Diperbarui 27 Mei 2024</b>
+            <b>*Diperbarui 01 Mei 2024</b>
         </p>
     </div>
 @endsection
