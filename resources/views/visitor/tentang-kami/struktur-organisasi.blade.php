22aa629bd60241a163536807fcf75dd84ef42a58
SeanAles <sean64971@gmail.com>
2024-06-16 23:15:00 +0900

    change struktur organisasi diagram

--- resources/views/visitor/tentang-kami/struktur-organisasi.blade.php
+++ resources/views/visitor/tentang-kami/struktur-organisasi.blade.php
@@ -7,7 +7,8 @@
         <h1>Struktur Organisasi</h1>
     </div>
     <div class="container">
-        <img src="{{ asset('visitor/assets/img/diagram/Diagram Struktur Organisasi RS (27 Mei 2024) (4).png') }}" alt="" width="100%" height="100%">
+        <img src="{{ asset('visitor/assets/img/diagram/Diagram Struktur Organisasi RS(16 Juni 2024).png') }}" alt=""
+            width="100%" height="100%">
         <p class="text-danger">
             <b>*Diperbarui 01 Mei 2024</b>
         </p>
