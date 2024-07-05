d1e4de87a2b013b98644e0e45097a8028f6d38c9
SeanAles <sean64971@gmail.com>
2024-07-05 22:19:01 +0900

    adding some images to promkes and change iklan image

--- resources/views/visitor/beranda/beranda.blade.php
+++ resources/views/visitor/beranda/beranda.blade.php
@@ -57,7 +57,7 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
                 <div class="modal-body">
-                    <img src="{{ asset('visitor/assets/img/iklan/Idul Adha Haulussy 2024.jpg') }}" class="img-fluid"
+                    <img src="{{ asset('visitor/assets/img/iklan/Promkes Iklan.jpg') }}" class="img-fluid"
                         alt="Gambar Modal">
                 </div>
             </div>
