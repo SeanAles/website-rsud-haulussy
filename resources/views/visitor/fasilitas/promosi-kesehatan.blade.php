bf73435dab0125e7b077253328f04b0eee56c6dc
simrshaulussy2 <simrshaulussy@gmail.com>
2024-09-09 14:30:17 +0900

    delete some images in promkes page

--- resources/views/visitor/fasilitas/promosi-kesehatan.blade.php
+++ resources/views/visitor/fasilitas/promosi-kesehatan.blade.php
@@ -9,23 +9,16 @@
 
     <div class="container">
         <div class="row justify-content-center">
-            <div class="text-center col-12 col-sm-12 col-md-8 col-lg-4 mt-3 mb-3">
-                <img src="{{ asset('visitor/assets/img/promkes/Selamat Hari Bidan.png') }}" width="100%" alt="Promkes 1"><br>
+           
+            <div class="text-center col-12 col-sm-12 col-md-8 col-lg-4 mt-3">
+                <img src="{{ asset('visitor/assets/img/promkes/Promkes Long Banner.jpg') }}" width="100%"
+                    alt="Promkes 2"><br>
             </div>
             <div class="text-center col-12 col-sm-12 col-md-8 col-lg-4 m-0 p-0">
                 <img src="{{ asset('visitor/assets/img/gallery/qr_code.png') }}" width="80%" alt="Promkes QR"><br>
                 <p class="mt-2"><b>https://tiny.one/Edukasi-Haulussy</b></p>
                 <a href="https://tiny.one/Edukasi-Haulussy" target="_blank" class="btn btn-info mb-3">Tekan Saya</a>
             </div>
-            <div class="text-center col-12 col-sm-12 col-md-8 col-lg-4 mt-3 mb-3">
-                <img src="{{ asset('visitor/assets/img/promkes/Senam Pagi Promkes.jpg') }}" width="100%"
-                    alt="Promkes 1"><br>
-            </div>
-            <div class="text-center col-12 col-sm-12 col-md-8 col-lg-4 mt-3">
-                <img src="{{ asset('visitor/assets/img/promkes/Promkes Long Banner.jpg') }}" width="100%"
-                    alt="Promkes 2"><br>
-            </div>
-
             <div class="text-center col-12 col-sm-12 col-md-8 col-lg-4 mt-3">
                 <img src="{{ asset('visitor/assets/img/iklan/Promkes Iklan.jpg') }}" width="100%" alt="Promkes 4"><br>
             </div>
