e14c86896dc0d9dc4f0eab789d9f166402698164
SeanAles <sean64971@gmail.com>
2024-09-28 11:12:15 +0900

    update some content in promkes page

--- resources/views/visitor/fasilitas/promosi-kesehatan.blade.php
+++ resources/views/visitor/fasilitas/promosi-kesehatan.blade.php
@@ -2,6 +2,22 @@
 
 @section('title', 'Promosi Kesehatan')
 
+@section('style')
+    <style>
+        .restream-button {
+            font-size: 15px;
+            color: black;
+        }
+
+        .restream-text {
+            color: #283779;
+            font-weight: 500;
+        }
+    </style>
+@endsection
+
+
+
 @section('content')
     <div class="text-center mb-5">
         <h1>Promosi Kesehatan</h1>
@@ -9,7 +25,7 @@
 
     <div class="container">
         <div class="row justify-content-center">
-           
+
             <div class="text-center col-12 col-sm-12 col-md-8 col-lg-4 mt-3">
                 <img src="{{ asset('visitor/assets/img/promkes/Promkes Long Banner.jpg') }}" width="100%"
                     alt="Promkes 2"><br>
@@ -21,6 +37,26 @@
             </div>
             <div class="text-center col-12 col-sm-12 col-md-8 col-lg-4 mt-3">
                 <img src="{{ asset('visitor/assets/img/iklan/Promkes Iklan.jpg') }}" width="100%" alt="Promkes 4"><br>
+            </div>
+            <div class="text-center col-12 col-sm-12 col-md-8 col-lg-4 mt-3">
+                <img src="{{ asset('visitor/assets/img/iklan/Hari Rabies Sedunia.jpeg') }}" width="100%"
+                    alt="Promkes 1"><br>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="text-center col-12 col-sm-12 col-md-8 col-lg-4 mt-3">
+                <a href="https://drive.google.com/file/d/1iy9OK3EFvJ4VFEq-KqTTOGCXO8SPlH1M/view?usp=drivesdk">
+                    <img src="{{ asset('visitor/assets/img/iklan/Potensi Masalah Saraf Akibat Stunting.jpeg') }}"
+                        width="100%" alt="Promkes 3">
+                </a>
+                <br>
+                <div class="container">
+                    <p class="restream-button">Dengarkan Ulang? <span><a
+                                href="https://drive.google.com/file/d/1iy9OK3EFvJ4VFEq-KqTTOGCXO8SPlH1M/view?usp=drivesdk"
+                                class="restream-text">Klik
+                                Disini</a></span></p>
+                </div>
             </div>
         </div>
     </div>
@@ -37,17 +73,17 @@
             </tr>
 
             @foreach ($promkes as $artikelLuar)
-            <tr class="text-black">
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $artikelLuar->name }}</td>
-                <td>
-                    <a target="_blank" class="download-button" href="{{ $artikelLuar->url }}">Lihat Artikel</a>
-                </td>
-            </tr>
+                <tr class="text-black">
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $artikelLuar->name }}</td>
+                    <td>
+                        <a target="_blank" class="download-button" href="{{ $artikelLuar->url }}">Lihat Artikel</a>
+                    </td>
+                </tr>
             @endforeach
-    
-           
-         
+
+
+
         </table>
     </div>
 @endsection
