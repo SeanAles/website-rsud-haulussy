370efe5040ff750f2b2a27244b2194a5b5e00309
simrshaulussy2 <simrshaulussy@gmail.com>
2024-12-13 09:21:09 +0900

    update iklan

--- resources/views/visitor/beranda/beranda.blade.php
+++ resources/views/visitor/beranda/beranda.blade.php
@@ -57,7 +57,7 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
                 <div class="modal-body">
-                    <img src="{{ asset('visitor/assets/img/iklan/natal.jpg') }}" class="img-fluid"
+                    <img src="{{ asset('visitor/assets/img/iklan/piagam.jpg') }}" class="img-fluid"
                         alt="Gambar Modal">
                 </div>
             </div>
