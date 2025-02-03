248c8cd0fa9d312b3e527cc328bbf2a8c0b074b5
01 <simrshaulussy@gmail.com>
2025-02-03 14:56:31 +0900

    update subtitle tarif 3.2

--- resources/views/visitor/layout/main.blade.php
+++ resources/views/visitor/layout/main.blade.php
@@ -221,7 +221,7 @@
                             <a class="dropdown-item" href="/medical-check-up">Medical Check Up</a> --}}
                             <a class="dropdown-item" href="/ketersediaan-tempat-tidur">Ketersediaan Tempat
                                 Tidur</a>
-                            <a class="dropdown-item" href="/tarif-pelayanan">Tarif Pelayanan <span class="notif">*</span></a>
+                            <a class="dropdown-item" href="/tarif-pelayanan">Tarif Pelayanan</a>
                             <a class="dropdown-item" href="/promosi-kesehatan">Promosi Kesehatan</a>
                             <a class="dropdown-item" href="/tata-tertib">Tata Tertib</a>
                             <div class="dropdown-divider"></div>
