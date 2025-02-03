fdd0a1fc75cdf478ea583c9e66dbc50635d1c78c
01 <simrshaulussy@gmail.com>
2025-02-03 14:47:58 +0900

    update subtitle tarif 2

--- resources/views/visitor/fasilitas/tarif-pelayanan.blade.php
+++ resources/views/visitor/fasilitas/tarif-pelayanan.blade.php
@@ -23,7 +23,7 @@
 @section('content')
     <div class="text-center mb-5">
         <h1>Tarif Pelayanan</h1>
-        <p>PERATURAN GUBERNUR MALUKU NOMOR 37 TAHUN 2024 (05 November 2024) TENTANG PENETAPAN TARIF RETRIBUSI DAERAH (Tarif Baru RSUD dr. M. Haulussy Ambon). </p>
+        <p>PERATURAN GUBERNUR MALUKU NOMOR 37 TAHUN 2024 (05 November 2024) TENTANG PENETAPAN TARIF RETRIBUSI DAERAH (Tarif Baru RSUD dr. M. Haulussy Ambon) </p>
     </div>
     <div class="container d-flex justify-content-center">
         <select id="room_id">
